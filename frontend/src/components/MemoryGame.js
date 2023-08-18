06631e2958209c3d0f6ed6a777b47698d216d9f4
ZephyrOne <bjshenshijun@yahoo.com>
2023-08-17 23:45:37 -0700

    Added memory game demo

---
+++ frontend/src/components/MemoryGame.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import './MemoryGame.css';
+
+function MemoryGame() {
+  const cards = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D'];
+  const [shuffledCards, setShuffledCards] = useState([]);
+  const [flippedIndices, setFlippedIndices] = useState([]);
+  const [matchedIndices, setMatchedIndices] = useState([]);
+
+  useEffect(() => {
+    setShuffledCards(shuffle([...cards]));
+  }, []);
+
+  const handleCardClick = (index) => {
+    if (flippedIndices.includes(index) || matchedIndices.includes(index)) return;
+
+    if (flippedIndices.length === 2) {
+      setTimeout(() => setFlippedIndices([index]), 1000);
+    } else {
+      setFlippedIndices(prev => [...prev, index]);
+    }
+
+    if (flippedIndices.length === 1) {
+      const [firstCardIndex] = flippedIndices;
+      if (shuffledCards[firstCardIndex] === shuffledCards[index]) {
+        setMatchedIndices(prev => [...prev, firstCardIndex, index]);
+        setFlippedIndices([]);
+      }
+    }
+  }
+
+  return (
+    <div className="memory-game">
+      <div className="board">
+        {shuffledCards.map((card, index) => (
+          <div
+            key={index}
+            className="card"
+            onClick={() => handleCardClick(index)}
+          >
+            {(flippedIndices.includes(index) || matchedIndices.includes(index)) ? card : "?"}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+export default MemoryGame;
